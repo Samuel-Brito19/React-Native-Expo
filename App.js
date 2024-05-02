46e2ea1db66e0f585cc8cf5beb119f64000735ec
Samuel-Brito19 <sambriton7@gmail.com>
2024-05-01 21:31:11 -0300

    Starting project with expo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Why it's not typescript?</Text>
       <StatusBar style="auto" />
     </View>
   );
