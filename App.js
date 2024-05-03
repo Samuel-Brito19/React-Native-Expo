5a392bc5e1e156fe9104617515c145cb2f4a4001
Samuel-Brito19 <sambriton7@gmail.com>
2024-05-03 13:15:11 -0300

    Button file

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import ImageViewer from './components/ImageViewer';
 
 const PlaceholderImage = require('./assets/images/background-image.png')
 
@@ -7,7 +8,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <Text style={{color: '#fff'}}>Why it's not typescript?</Text>
       <StatusBar style="auto" />
@@ -26,5 +27,4 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 58,
   },
-  
 });
