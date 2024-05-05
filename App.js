140a8d5b4ce64d29a1705a6c5df7535768910762
Samuel-Brito19 <sambriton7@gmail.com>
2024-05-05 10:21:10 -0300

    Selecting a image from the device

--- App.js
+++ App.js
@@ -3,18 +3,21 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
 import * as ImagePicker from 'expo-image-picker'
+import { useState } from 'react';
 
 const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
-  const pickerImageAsync = async() => {
+  const [selectedImage, setSelectedImage] = useState(null)
+  
+  const pickImageAsync = async() => {
     let result = await ImagePicker.launchCameraAsync({
       allowsEditing: true,
       quality: 1,
     })
 
     if(!result.canceled) {
-      console.log(result)
+      setSelectedImage(result.assets[0].uri)
     } else {
       alert('You did not select any image.')
     }
@@ -22,10 +25,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary"/>
+        <Button label="Choose a photo" theme="primary" onPress={pickImageAsync}/>
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
