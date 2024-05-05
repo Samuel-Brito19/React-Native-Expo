b478aa91bfc8cdb3ca83b8aa9d5cc3a19d4b21c2
Samuel-Brito19 <sambriton7@gmail.com>
2024-05-05 10:42:56 -0300

    Creating CircleButton

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ const PlaceholderImage = require('./assets/images/background-image.png')
 
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null)
+  const [showAppOptions, setShowAppOptions] = useState(false)
   
   const pickImageAsync = async() => {
     let result = await ImagePicker.launchCameraAsync({
@@ -18,6 +19,7 @@ export default function App() {
 
     if(!result.canceled) {
       setSelectedImage(result.assets[0].uri)
+      setShowAppOptions(true)
     } else {
       alert('You did not select any image.')
     }
@@ -27,12 +29,14 @@ export default function App() {
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
       </View>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary" onPress={pickImageAsync}/>
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (<View/>) : (
+        <View style={styles.footerContainer}>
+          <Button label="Choose a photo" theme="primary" onPress={pickImageAsync}/>
+          <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
+        </View>
+      )}
       <StatusBar style="auto" />
-    </View>
+      </View>
   );
 }
 
